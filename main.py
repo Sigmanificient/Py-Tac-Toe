c2327a79f31bae853ff2b0cb6c2b493e7ef8cd50
Yohann Boniface <edhyjox@gmail.com>
2021-07-02 23:54:49 +0200

    property + adding magic methods

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ class Board:
             [None, None, None]
         ]
 
+    def __getitem__(self, item):
+        pass
+
+    def __setitem__(self, item, value):
+        pass
+
+    def __repr__(self):
+        return '\n'.join(' '.join(symbol or ' ' for symbol in line) for line in self.board)
+
     def is_empty(self, index):
         return
 
@@ -20,18 +29,21 @@ class Board:
 
         # TODO: set box oof index to correct symbol
 
+    @property
     def is_win(self):
         """Check if there is a win condition."""
-        return self.is_line() or self.is_column() or self.is_diagonals()
+        return self.is_diagonals or self.is_line or self.is_column
 
+    @property
     def is_line(self):
-        pass
-        # TODO: for each line of the board, check if 3 time same symbol
+        return any(a == b == c and a is not None for a, b, c in self.board)
 
+    @property
     def is_column(self):
         """For every column of the board, checks if 3 time same symbol."""
         return any(a == b == c and a is not None for a, b, c in zip(*self.board))
 
+    @property
     def is_diagonals(self):
         """Check if one of the two diagonals have the 3 same symbol """
         if self.board[1][1] is None:
@@ -45,13 +57,6 @@ class Board:
 
         return False
 
-    def show(self):
-        print(
-            '\n'.join(
-                ' '.join(symbol or ' ' for symbol in line) for line in self.board
-            )
-        )
-
 
 class Player:
 
@@ -62,17 +67,16 @@ class Player:
         pass
 
         # TODO: ask where to play
-        # TODO: return the choice
+        # TODO: if choice not valid  (1..9) => retry
 
 
 class Game:
 
     def __init__(self):
         self.board = Board()
-        self.players = [...]  # TODO: set 2 players with an unique symbols
+        self.players = [Player('x'), Player('o')]
 
         if random.randint(0, 1):
-            # Reversing players list to change who plays first
             self.players = self.players[::-1]
 
         self.is_running = True
@@ -82,10 +86,11 @@ class Game:
 
             for player in self.players:
                 print(self.board)
-                _ = player.choose()
+                index = player.choose()
 
-                # TODO: While index is not valid, ask again
-                # TODO: set board index.
+                while not self.board.is_empty(index):
+                    print("Please select a empty box !")
+                    index = player.choose()
 
                 if self.board.is_win():
                     print(f"{player.symbol} won the game !")
